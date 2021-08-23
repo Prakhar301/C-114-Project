96b9805dec0946d730e1e1789e63f93fb0ae8201
Prakhar301 <84377317+Prakhar301@users.noreply.github.com>
2021-08-23 14:19:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+}
+function setup(){
+canvas=createCanvas(450,450);
+canvas.position(500,125);
+video=createCapture(VIDEO);
+video.hide();
+}
+function take_snapshot(){
+}
+function draw(){
+}
